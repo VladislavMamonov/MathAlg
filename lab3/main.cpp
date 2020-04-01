826d3354a1996bb4a4124a15dd948aceea7cde93
Vladislav Mamonov <VMwork0123@gmail.com>
2020-04-01 18:31:32 +0700

    1

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -6,7 +6,7 @@ int check_data(equation obj)
   double Fa = obj.x1 * pow(obj.a, obj.x1Degree) + obj.x2 * pow(obj.a, obj.x2Degree) + obj.Const;
   double Fb = obj.x1 * pow(obj.b, obj.x1Degree) + obj.x2 * pow(obj.b, obj.x2Degree) + obj.Const;
 
-  if (Fa * Fb > 0)
+  if (Fa * Fb >= 0)
     return 1;
 
   return 0;
@@ -15,20 +15,20 @@ int check_data(equation obj)
 
 equation test_data(equation obj)
 {
-  obj.x1Degree = 2;
-  obj.x2Degree = 1;
-  obj.x1 = 1, obj.x2 = -1, obj.Const = -4;
-  obj.a = 2, obj.b = 3;
+  // obj.x1Degree = 2;
+  // obj.x2Degree = 1;
+  // obj.x1 = 1, obj.x2 = -1, obj.Const = -4;
+  // obj.a = 1, obj.b = 2;
 
   // obj.x1Degree = 2;
   // obj.x2Degree = 1;
   // obj.x1 = 5, obj.x2 = -4, obj.Const = 1;
   // obj.a = 0, obj.b = 10;
 
-  // obj.x1Degree = 2;
-  // obj.x2Degree = 1;
-  // obj.x1 = 1, obj.x2 = 12, obj.Const = 8;
-  // obj.a = -5, obj.b = 5;
+  obj.x1Degree = 2;
+  obj.x2Degree = 1;
+  obj.x1 = 1, obj.x2 = 12, obj.Const = 8;
+  obj.a = -5, obj.b = 5;
 
 
   return obj;
@@ -40,5 +40,5 @@ int main()
   equation obj;
 
   obj = test_data(obj);
-  bisection_process(obj);
+  chord_process(obj);
 }
